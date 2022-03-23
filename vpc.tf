8b73ebea6d7401b95e3b7dd837580118312c5d86
Rajawi73 <96315522+Rajawi73@users.noreply.github.com>
2022-03-23 02:18:04 -0500

    modified vpc

--- vpc.tf
+++ vpc.tf
@@ -2,21 +2,3 @@ resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
   tags       = var.tags
 }
-
-
-resource "aws_subnet" "private1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.vpc_subnets.cidr[0]
-}
-
-
-resource "aws_subnet" "private2" {  
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.vpc_subnets.cidr[1]
-}
-
-
-resource "aws_subnet" "private3" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.vpc_subnets.cidr[2]
-}

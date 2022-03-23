cab0d3977abebf502ffa095c7b6815353d8c8430
Rajawi73 <96315522+Rajawi73@users.noreply.github.com>
2022-03-23 09:40:51 -0500

    added public subnet

---
+++ public_subnet.tf
@@ -0,0 +1,11 @@
+resource "aws_subnet" "public1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.vpc_subnets.cidr[0]
+
+  resource "aws_subnet" "public2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.vpc_subnets.cidr[1]
+
+  resource "aws_subnet" "public3" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.vpc_subnets.cidr[2]
